73f696ca13a0f568db59007fc92cc0160a570026
Ajay Kumar <ajaykumar201761@gmail.com>
2020-04-26 16:16:00 +0530

    Delete quote

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -37,7 +37,7 @@ class QuoteCard extends StatelessWidget {
           FlatButton.icon(
               onPressed: delete,
               icon: Icon(Icons.delete),
-              label: Text('Delete Quote')
+              label: Text('Delete quote')
           )
         ],
       ),
